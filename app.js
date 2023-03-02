6ed87752bd9f70f969032bbe2bccb0bff415aaa5
Ichihanketsu <37333451+Ichihanketsu@users.noreply.github.com>
2023-03-02 15:32:56 +0200

    wip: update

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
